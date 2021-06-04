016b56bc5f5602627448de699c28c4f5d36a6c41
shaik abbas <shaikabbas051@gmail.com>
2021-06-04 21:30:54 +0530

    initial commit

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+// markup
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <h1>This is contacts page</h1>
+    </main>
+  );
+};
+
+export default IndexPage;
